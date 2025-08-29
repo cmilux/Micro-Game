2572687200f0bea15df4c4acb2dddd8f5ea6c131
cmilux <villa12marti@gmail.com>
2025-08-29 01:23:20 -0300

    UI added
    Game Over text was added at the end of the game
    TMP was imported

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private Rigidbody2D _playerRb;
 
+    public GameObject gameOver;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,5 +26,13 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         _playerRb.linearVelocityX = horizontalInput * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            gameOver.SetActive(true);
+        }
     }
 }
